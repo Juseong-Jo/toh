6b95ddacb5e6adc872f950ab107fb7ed9a00268a
geine <jyyd56@gmail.com>
2018-02-03 12:22:48 +0900

    2018-02-03 literal 객체 생성 및 바인딩

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Hero} from '../hero';
+
 
 @Component({
   selector: 'app-heroes',
@@ -11,13 +13,19 @@ export class HeroesComponent implements OnInit {
   // hero2: string = 'win storm'; 타입지정 자동삽입
   // public hero3 = 'win storm'; public이 기본값
 
-  // 클래스 객체로 생성
-  hero = {
-    id: 1,
-    name: 'Win Strom'
-  }
+  // 일반객체 생성문법
+  // hero = {
+  //   id: 1,
+  //   name: 'Win Strom'
+  // };
 
-  constructor() { }
+  // 클래스 객체로 생성
+  hero: Hero = {
+    id: 1,
+    name: 'Win Storm'
+  };
+
+  constructor() {}
 
   ngOnInit() {
   }
